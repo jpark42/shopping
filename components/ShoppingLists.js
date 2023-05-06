f541c7955972b8dc6a87edca2c524295919b43a5
Johnny Park <110794898+jpark42@users.noreply.github.com>
2023-05-06 17:37:09 -0400

    Add Functionality to Query into Firebase from App
    - users can add lists into the database and it will render in app

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,16 +1,29 @@
-import { View, FlatList, StyleSheet, Text } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  View,
+  FlatList,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 import { useState, useEffect } from "react";
 // import firebase functions for querying data
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 //Create and export the component, then destructuring the props "db" object that’s passed to the component
 const ShoppingLists = ({ db }) => {
   const [lists, setLists] = useState([]);
+  const [listName, setListName] = useState("");
+  const [item1, setItem1] = useState("");
+  const [item2, setItem2] = useState("");
 
   const fetchShoppingLists = async () => {
     /*db - database object passed as props from App.js
     shoppinglists - name of the collection in firebase*/
-    const listsDocuments = await getDocs(collection(db, "shoppinglists"));
+    const listsDocuments = await getDocs(collection(db, "shopppinglists"));
 
     //creating an empty array "newLists" that will contain new data filled by the forEach() loop
     let newLists = [];
@@ -35,18 +48,82 @@ const ShoppingLists = ({ db }) => {
     fetchShoppingLists();
   }, [`${lists}`]);
 
+  const addShoppingList = async (newList) => {
+    /*addDoc will add new document to collection and generate id
+    we use db - database intialised in the App.js
+    shoppinglists - name of the collection
+    newList - object created from user inputs (listName, item1, item2)*/
+
+    const newListRef = await addDoc(collection(db, "shopppinglists"), newList);
+    /*If new document was successfuly created .id and .data() will be set on it.
+    if newListRef.id exists (true) - alert success message
+    if newListRef.id doesn't exist (false) - alert fail message*/
+
+    if (newListRef.id) {
+      /*setLists setter function to update lists state
+      1.`${lists}` which is a dependency array of useEffect will change
+      2. the userEffect code will be executed 
+      3. fetchShoppingLists will be called again, fetching the updated data of shoppinglists collection from the database
+      */
+      setLists([newList, ...lists]);
+
+      Alert.alert(`The list '${listName}' has been added.`);
+    } else {
+      Alert.alert(`Unable to add. Please try later`);
+    }
+  };
+
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
       {/** Flatlist is a react component that renders basic lists and works best with data that changes over time */}
       <FlatList
+        style={styles.listsContainer}
         data={lists}
         renderItem={({ item }) => (
-          <Text>
+          <View style={styles.listItem}>
             {/** join() converts an array into a string. It also adds a separator string between each element from the array once concatenated */}
-            {item.name}: {item.items.join(", ")}
-          </Text>
+            <Text>
+              {item.name}: {item.items.join(", ")}
+            </Text>
+          </View>
         )}
       />
+      <View style={styles.listForm}>
+        <TextInput
+          style={styles.listName}
+          placeholder="List Name"
+          value={listName}
+          onChangeText={setListName}
+        />
+        <TextInput
+          style={styles.item}
+          placeholder="Item #1"
+          value={item1}
+          onChangeText={setItem1}
+        />
+        <TextInput
+          style={styles.item}
+          placeholder="Item #2"
+          value={item2}
+          onChangeText={setItem2}
+        />
+        {/** Creates new object out of 3 state's values and then calls addShoppingList function defined above*/}
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => {
+            const newList = {
+              name: listName,
+              items: [item1, item2],
+            };
+            addShoppingList(newList);
+          }}
+        >
+          <Text style={styles.addButtonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      {Platform.OS === "ios" ? (
+        <KeyboardAvoidingView behavior="padding" />
+      ) : null}
     </View>
   );
 };
@@ -54,6 +131,51 @@ const ShoppingLists = ({ db }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  listItem: {
+    height: 70,
+    justifyContent: "center",
+    paddingHorizontal: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: "#AAA",
+    flex: 1,
+    flexGrow: 1,
+  },
+  listForm: {
+    flexBasis: 275,
+    flex: 0,
+    margin: 15,
+    padding: 15,
+    backgroundColor: "#CCC",
+  },
+  listName: {
+    height: 50,
+    padding: 15,
+    fontWeight: "600",
+    marginRight: 50,
+    marginBottom: 15,
+    borderColor: "#555",
+    borderWidth: 2,
+  },
+  item: {
+    height: 50,
+    padding: 15,
+    marginLeft: 50,
+    marginBottom: 15,
+    borderColor: "#555",
+    borderWidth: 2,
+  },
+  addButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    backgroundColor: "#000",
+    color: "#FFF",
+  },
+  addButtonText: {
+    color: "#FFF",
+    fontWeight: "600",
+    fontSize: 20,
   },
 });
 
